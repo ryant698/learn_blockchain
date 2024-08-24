247ed8420805f984ad818fca66fb5ac3b955b564
hungcuongthan <67635895+hungcuongthan@users.noreply.github.com>
2024-08-24 14:54:15 +0700

    update

--- lottery-react-boilerplate/lottery-react/src/App.js
+++ lottery-react-boilerplate/lottery-react/src/App.js
@@ -1,26 +1,66 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import web3 from "./web3";
+import lottery from "./lottery";
 
 class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { manager: "" };
+  // }
+
+  state = {
+    manager: "",
+    players: [],
+    balance: "",
+    value: "",
+    message: "",
+  };
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ manager: manager });
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ message: "Waiting on transaction success..." });
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, "ether"),
+      gas: "10000000",
+    });
+    this.setState({ message: "You have been successfully entered!" });
+  };
+
   render() {
+    lottery.methods.manager().call().then(console.log);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>Lottery Contract</h2>
+        <p>Manager of this contract is {this.state.manager}</p>
+        <p>
+          There are currently {this.state.players.length} people entered,
+          competing to win {web3.utils.fromWei(this.state.balance, "ether")}{" "}
+          ether!
+        </p>
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to join the lottery?</h4>
+          <div>
+            <label>Amount of ether to enter</label>
+            <input
+              type="float"
+              value={this.state.value}
+              onChange={(event) => this.setState({ value: event.target.value })}
+            />
+          </div>
+          <button>Enter</button>
+        </form>
+        <hr />
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
---
+++ lottery-react-boilerplate/lottery-react/src/lottery.js
@@ -0,0 +1,75 @@
+import web3 from "./web3";
+
+const address = "0xc1A1D2ed0A347B4474E83d01C2429858F4632324";
+
+const abi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getPlayers",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "manager",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "players",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+// Create a contract instance of the real contract on the blockchain
+// or we can view it as a representation in local
+// A portal that we can interact with the contract on blockchain
+export default new web3.eth.Contract(abi, address);
