366d3c9257c57f11b5030d9cba5a5facb777c52f
hungcuongthan <hungcuongthan@gmail.com>
2024-09-01 19:18:58 +0700

    add .env file

--- test_astx/web3.js
+++ test_astx/web3.js
@@ -1,6 +1,9 @@
 const { Web3 } = require("web3"); // Replace with your Infura Project ID or provider URL
-const infuraUrl =
-  "https://mainnet.infura.io/v3/71122d970feb4a3f9bb2c5e4e4fcef58";
+require("dotenv").config();
+require("dotenv").config();
+
+const ETH_URL = process.env.ETH_URL;
+const infuraUrl = ETH_URL;
 const web3 = new Web3(infuraUrl);
 
 module.exports = web3;
